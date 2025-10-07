93a4c3359493b27a113f8c88bb348e8eb055d677
gris2004 <bautistatris2@gmail.com>
2025-10-06 19:57:30 -0500

    todos los archvios para hacer la prueba del .gitignore

--- GUI/home_frame.py
+++ GUI/home_frame.py
@@ -1,6 +1,6 @@
-import tkinter as tk
+﻿import tkinter as tk
 from tkinter import ttk
-
+#a
 #inicialización y configuración de frameHome
 homeFrame = tk.Tk()
 homeFrame.geometry("500x500")
@@ -10,6 +10,6 @@ frameLayout = tk.ttk.Frame(homeFrame, padding = 10)
 frameLayout.grid()
 
 inventoryButton = tk.Button(homeFrame, text = "Inventario").grid(row = 1, column = 1)
-tiketHistoriesButton = tk.Button(homeFrame, text = "Historial de tikets").grid(row = 1, column = 1)
-salesButton = tk.Button(homeFrame, text = "Punto de Venta").grid(row = 1, column = 1)
+tiketHistoriesButton = tk.Button(homeFrame, text = "Historial de tikets").grid(row = 2, column = 1)
+salesButton = tk.Button(homeFrame, text = "Punto de Venta").grid(row = 3, column = 1)
 homeFrame.mainloop()
