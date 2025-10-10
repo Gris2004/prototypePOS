664929de0814de985c6e8ae11205716d6b477c83
gris2004 <bautistatris2@gmail.com>
2025-10-10 16:01:58 -0500

    el error de home_frame.py se ha solucionado y ha habido un pequeño cambio en el código

--- GUI/home_frame.py
+++ GUI/home_frame.py
@@ -12,12 +12,11 @@ class HomeFrame:
     def callNewFrame(self, name):
         """Llama a un frame existente por su nombre"""
         #DEUDA TÉCNICA: Encontrar una forma de automatizar esta función sin necesidad de usar if's
-        genericFrame = 0
         if (name == "tikets"):
-            tikets = TiketsFrame(genericFrame, "500x500", "Historial de Tikets")
+            tikets = TiketsFrame(self.frame, "500x500", "Historial de Tikets")
             tikets.showTiketsFrame()
         if (name == "inventory"):
-            inventory = InventoryFrame(genericFrame, "500x500", "Inventario")
+            inventory = InventoryFrame(self.frame, "500x500", "Inventario")
             inventory.showInventoryFrame()
 
     def showHomeFrame(self):
@@ -26,12 +25,14 @@ class HomeFrame:
         self.frame.geometry(self.geometry)
         self.frame.title(self.title)
 
-        inventoryButton = tk.Button(self.frame, text = "Inventario", command = self.callNewFrame("inventory")).grid(row = 1, column = 1)
-        tiketHistoriesButton = tk.Button(self.frame, text = "Historial de tikets", command = self.callNewFrame("tikets")).grid(row = 2, column = 1)
+        inventoryButton = tk.Button(self.frame, text = "Inventario", command = lambda: self.callNewFrame("inventory")).grid(row = 1, column = 1)
+        tiketHistoriesButton = tk.Button(self.frame, text = "Historial de tikets", command = lambda: self.callNewFrame("tikets")).grid(row = 2, column = 1)
         salesButton = tk.Button(self.frame, text = "Punto de Venta").grid(row = 3, column = 1)
         self.frame.mainloop()
 
 #inicialización y configuración de frameHome
 home_frame = 0
-home = HomeFrame(home_frame, "500x500", "Ventana Inicial")
-home.showHomeFrame()
+
+if __name__ == "__main__":
+	home = HomeFrame(None, "500x500", "Ventana Inicial")
+	home.showHomeFrame()
