fcfa7b01c7e359bf8e3e897ccbd3ba7fae1c229c
gris2004 <bautistatris2@gmail.com>
2025-10-14 19:22:30 -0500

    update in api/db/dbconnection.mjs: created of a function called createTable

--- API/db/dbconnection.mjs
+++ API/db/dbconnection.mjs
@@ -8,6 +8,9 @@ let columsNameArray = []
 //habilitar el modo de depuración
 sqlite3.verbose();
 
+/**
+*connectToDatabase es una función que te permite conectarte al archivo SalesPoint.db
+*/
 //función para conectar a base de datos
 async function connectToDatabase() {
 	//abriendo base de datos
@@ -20,7 +23,27 @@ async function connectToDatabase() {
 }
 
 //función para crear tablas
-
+/**
+*create tables sirve para crear tablas con2 parametros
+*@param {string} ableName - el nombre de la tabla
+*@param {string arry} columnsArray - arreglo de las columnas a crear
+*/
+async function createTable(tableName, columnsArray){
+	if (!Array.isArray(columnsArray) || columnsArray.length === 0){
+		throw new Error(console.log("El arreglo de columnas no puede estar vacia"));
+	}	
+	
+	if (!/^[a-zA-Z0-9_]+$/.test(tableName)){
+		throw Error(console.log("Nombre de tabla no valido"));
+	}
+	const columns = join(", ") //converte "id", "integer" en "name TEXT" en "id INTEGER, name TEXT"
+	try{
+		const db = await connectToDatabase();	
+		await db.run(`CREATE TABLE ${tableName} (${columns})`);
+	} catch (error) {
+		console.log("error al crear la tabla", error);
+	}
+}
 
 //se llama a la función con un catch para caso de errores
 await connectToDatabase().catch((err) => console.log(`error al conectar con la base de datos: ${err}`));
