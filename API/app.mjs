90ccb374c4ca849ab3ff2111c361bf84eb2f4f27
Tristan Cruz <bautistatris2@gmail.com>
2025-10-16 18:42:31 -0600

    update app.mjs and salespoint.db was created

--- API/app.mjs
+++ API/app.mjs
@@ -1,16 +1,18 @@
 //importación de librerias
 import express from 'express';
-import router from './routes/routes.mjs'
+import router from './routes/routes.mjs';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 //variables del servidor
-const port = process.env.port || 8080;
-const ip = process.env.ip || 'localhost';
+const port = process.env.API_PORT || 8080;
+const ip = process.env.HOME_LOCAL_IP || 'localhost';
 
 app.use('/api', router);
 app.set('json spaces', 2);
 
 
 //configuración e inicialización del servidor nodejs
-app.listen(port, ip);
+app.listen(port, ip, () => console.log(`listening port: ${port}, and ip: ${ip}`));
--- API/routes/routes.mjs
+++ API/routes/routes.mjs
@@ -4,20 +4,20 @@ var router = Router();
 
 //endpoint para consultar las tablas
 router.get('/tables/consult', async(req, res) => {
-    res.json({"message": "this is the consult endpoint to watch the tables"})
+    res.json({"message": "this is the consult endpoint to watch the tables"});
 });
 
 //endpoint para crear un registro
 router.post('/tables/create', async(req, res) => {
-    res.json({"message": "this is the endpoint to create a field in any table "})
+    res.json({"message": "this is the endpoint to create a field in any table "});
 });
 
 router.delete('/tables/delete', async(req, res) => {
-    res.json({"message": "this is the endpoint to delete a field in any table width an id"})
+    res.json({"message": "this is the endpoint to delete a field in any table width an id"});
 });
 
 router.post('/tables/update', async(req, res) => {
-    res.json({"message": "this is the endpoint to update a field in any table with an id or an script"})
+    res.json({"message": "this is the endpoint to update a field in any table with an id or an script"});
 });
 
 export default router
