89d14697b82b9a2019dcbb70ac76e11f6c9927c0
Tristan Cruz <bautistatris2@gmail.com>
2025-11-03 23:35:15 -0600

    changing the tablesController.mjs and testing the SalesPoint.db

--- API/controllers/tablesController.mjs
+++ API/controllers/tablesController.mjs
@@ -1,5 +1,22 @@
+//importing dependencies
 import { connect } from '../db/dbconnection.mjs';
 import { dbPath } from '../db/dbUtils.mjs';
-import dontenv from 'dotenv';
+import dotenv from 'dotenv';
 
-console.log(dbPath);
+//db const that will keep open in each transaction, when the client closes, the database will too
+const db = connect(dbPath);
+dotenv.config();
+
+/**
+ * fetchData show the rows from the selected table
+ * @param [string] tableName - the table name for execute the consult
+ * @return [string] [rows, fields] - the rows and fieds from the table
+ * */
+
+export function fetchData (tableName) {
+    const query = `SELECT * FROM ${tableName}`;
+    const rows = db.prepare(query).all;
+    return rows;
+}
+
+console.log(fetchData('testTable'));
