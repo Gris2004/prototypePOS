ca6f21749557391dc5d60de73ee4eeb6151f1e6a
Tristan Cruz <bautistatris2@gmail.com>
2025-11-28 20:07:59 -0600

    feat: controllers/untils/addApostrophes.mjs for add apostrophes to the arrays or strings for the databasew

---
+++ API/untils/addApostrophes.mjs
@@ -0,0 +1,11 @@
+/**
+ * add an aphostrophes to the string or an array
+ * @param {string|array} value - the value to that aphostrophes are added
+ * @return {string|array} newValue - the same value but with the apostrophes added*/
+export function addApostrophes (value) {
+    try {
+        
+    } catch (err) {
+        return `error message: ${err}`;
+    }
+}
