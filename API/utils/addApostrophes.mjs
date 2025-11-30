4fda13b01a9f9ecb19de271d404df4a91bd8a4a9
Tristan Cruz <bautistatris2@gmail.com>
2025-11-29 20:02:46 -0600

    fix: function addApostrophes was fixed for add apostrophes only to the values that are strings

--- API/utils/addApostrophes.mjs
+++ API/utils/addApostrophes.mjs
@@ -3,15 +3,20 @@
  * @param {string|array} value - the value to that aphostrophes are added
  * @return {string|array} newValue - the same value but with the apostrophes added*/
 export default function addApostrophes (value) {
-    if (Array.isArray(value) && value.every(item => typeof item === 'string')) {
-        let newValues = [];
-        for (let i = 0; i < value.length; i++) {
-            newValues.push(`'${value[i]}'`);
+    try {
+        if(Array.isArray(value)) {
+            let newValue = [];
+            for(let i = 0; i < value.length; i++){
+                if(typeof value[i] === "string") newValue.push(`'${value[i]}'`);
+                else newValue.push(value[i]);
+            }
+            return newValue
+        } 
+        else if (typeof value === "string") {
+            let newValue = "";
+            return `'${value}'`
         }
-        return newValues;
-    }
-    else if (typeof value === "string") {
-        let newValue = "";
-        return `'${value}'`
+    } catch (err) {
+        return `Error message: ${err}`;
     }
 }
