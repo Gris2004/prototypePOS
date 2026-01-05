800cf9a6fe1afb189e1f8f1bd4cdc7b3844655e6
Gris2004 <bautistatris2@gmail.com>
2026-01-04 19:37:00 -0600

    feat: destructionDetect in window_controller.py}

--- GUI/window_controller.py
+++ GUI/window_controller.py
@@ -1,1 +1,21 @@
 import tkinter as tk
+from tkinter import messagebox
+
+class WindowController:
+    """WindowController is a class that controls the frames with some functions to be imported to other frames
+    and use its functions
+    
+    methods:
+        openWindow: opens a new window when this function is called
+        destructionDetect: detects when the user closes a window and sends a message or returns to home_frame"""
+
+    def destructionDetect(self, frame):
+        """it detects when the user closes the window and open the welcome window"""
+        close = messagebox.askyesno(
+                message = "¿estás seguro de que quieeres cerrar la ventana?",
+                title = "Confirmación de Cierre",
+                icon = "info"
+                )
+        if close:
+            #checking the window closure
+            frame.destroy()
